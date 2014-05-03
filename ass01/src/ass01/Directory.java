bbe3390f5ed98c7f0da38232e6bd45383ee4ede1
Alexander Hess <qala@uni-koblenz.de>
2014-05-03 19:20:10 +0200

    vererbungskram

--- ass01/src/ass01/Directory.java
+++ ass01/src/ass01/Directory.java
@@ -6,15 +6,7 @@ public class Directory extends Entry{
 
 	public Directory(Directory parent, String name) {
 		super(parent);
-		this.name = name;
-	}
-	
-	public void setName(String name){
-		this.name = name;
-	}
-	
-	public String getName(){
-		return name;
+		this.setName(name);
 	}
 	
 	public boolean delete(){
