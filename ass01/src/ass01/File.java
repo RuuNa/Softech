a3b386a16a8370ddf031e0b3c0c449414933e113
Alexander Hess <qala@uni-koblenz.de>
2014-05-03 15:06:21 +0200

    Added name variables and access to those

--- ass01/src/ass01/File.java
+++ ass01/src/ass01/File.java
@@ -1,9 +1,20 @@
 package ass01;
 
 public class File extends Entry{
+	
+	private String name;
 
 	public File(Directory parent, String name) {
 		super(parent);
+		this.name = name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
 	}
 
 }
